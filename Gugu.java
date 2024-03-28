5390da86b0538579528ae184afd792a33a05df70
preeti-panda <preetiprangyapanda@gmail.com>
2024-03-28 19:48:29 +0530

    If else Statement

---
+++ Gugu.java
@@ -0,0 +1,12 @@
+class Gugu{
+    public static void main(String args[]){
+        int x=18;
+        int y=20;
+        if(y>x){
+            System.out.println("Y is greater than X");
+        }else{
+            System.out.println("X is greater tahn Y");
+        }
+    }
+
+}
