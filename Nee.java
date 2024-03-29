abbd4da97171b2a83d1782d139037ce2dba1b30d
preeti-panda <preetiprangyapanda@gmail.com>
2024-03-29 11:59:21 +0530

    Nested For loop

---
+++ Nee.java
@@ -0,0 +1,11 @@
+class Nee{
+    public static void main(String args[]){
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
