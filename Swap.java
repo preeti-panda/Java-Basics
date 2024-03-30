37010f345ff51c57a0c1777a5961b7570d33b407
preeti-panda <preetiprangyapanda@gmail.com>
2024-03-30 19:55:12 +0530

    Swapping function

--- Swap.java
+++ Swap.java
@@ -1,12 +1,14 @@
-public class Swap{
-    public static void main(String args[]){
-        int a = 5;
-        int b = 10;
-
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println("a = " +a);
-        System.out.println("b = " +b);
-    }
+public class Swap {
+  public static void swap(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+    System.out.println("Before swapping: x = " + x + ", y = " + y);
+    swap(x, y);
+    System.out.println("After swapping: x = " + x + ", y = " + y);
+  }
 }
