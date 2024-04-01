5cd478c601ea164091d15c9fa79b3f690e18c251
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-01 22:37:55 +0530

    Single inheitance

--- Shape.java
+++ Shape.java
@@ -1,44 +1,23 @@
-class Shape{
-    protected String color;
-    public Shape(String color) {
-        this.color = color;
-    }
-    public double calculateArea() {
-        return 0.0;
-    }
-    public void displayInfo() {
-        System.out.println("Shape color: " + color);
-    }
-}
-class Circle extends Shape {
-    private double radius;
-    public Circle(String color, double radius) {
-        super(color);
-        this.radius = radius;
-    }
-    public double calculateArea() {
-        return Math.PI * radius * radius;
-    }
+class Shapee {
+  public void area() {
+    System.out.println("Area calculation for generic shape");
+  }
 }
 class Rectangle extends Shape {
-    private double length;
-    private double width;
-    public Rectangle(String color, double length, double width) {
-        super(color);
-        this.length = length;
-        this.width = width;
-    }
-    public double calculateArea() {
-        return length * width;
-    }
+  int length;
+  int breadth;
+
+  public Rectangle(int length, int breadth) {
+    this.length = length;
+    this.breadth = breadth;
+  }
+  public void area() {
+    System.out.println("Rectangle area: " + length * length);
+  }
 }
 public class Main {
-    public static void main(String[] args) {
-        Circle circle = new Circle("Red", 5.0);
-        circle.displayInfo();
-        System.out.println("Circle area: " + circle.calculateArea());
-        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
-        rectangle.displayInfo();
-        System.out.println("Rectangle area: " + rectangle.calculateArea());
-    }
+  public static void main(String[] args) {
+    Shape shape = new Rectangle(5, 4);
+    shape.area(); 
+  }
 }
--- while.java
+++
@@ -1,8 +0,0 @@
-class While{
-    public static void main(String args[]){
-        int x=1;
-        while (x<=4)
-        System.out.println(x);
-        x++;
-    }
-}
