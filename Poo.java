8cf3689e7a43526cf301db9733fde29cb9464fec
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-02 23:24:03 +0530

    Add two numbers

---
+++ Poo.java
@@ -0,0 +1,11 @@
+public class Poo {
+
+    public static void main(String[] args) {
+      int num1 = 5;
+      int num2 = 10;
+      int sum = num1 + num2;
+  
+      System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+  }
+  
