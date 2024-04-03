048be77cc9ac47e1da0f85eed414ac5574cda64f
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-03 11:47:10 +0530

    Multidimensional Array

---
+++ c.java
@@ -0,0 +1,17 @@
+class c{
+    public static void main(String args[]){
+        int num=1;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                a [i][j]=num;
+                ++num;
+            }
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
