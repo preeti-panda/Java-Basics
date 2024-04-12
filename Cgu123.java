356689757b723d1190aa1cab24c704b4d0360346
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-12 22:30:13 +0530

    Parameterised Constructor

---
+++ Cgu123.java
@@ -0,0 +1,8 @@
+public class Cgu123 {
+    public static void main(String[] args) {
+        add(10,12); 
+    }
+    public static void add(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+}
