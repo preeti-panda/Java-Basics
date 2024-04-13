a742784545512eb5f65b75d98bbb89c05b1e1a9b
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-13 22:44:04 +0530

    If else

---
+++ Cgu1233.java
@@ -0,0 +1,11 @@
+public class Cgu1233 {
+    public static void main(String[] args) {
+        int x=20;
+        int y=10;
+        if(y>x){
+            System.out.println("Y is greater than x");
+        }else{
+            System.out.println("X is greater than y");
+        }
+    }
+}
