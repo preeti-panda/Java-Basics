61f40b483cc8184ed1fe34beeab411f49a047885
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-13 22:48:59 +0530

    If else (Multiple Conditions)

---
+++ Cgu12345.java
@@ -0,0 +1,16 @@
+public class Cgu12345 {
+    public static void main(String[] args) {
+        int mark=65;
+        if(marks<50){
+            System.out.println("Fail");
+        }
+        else if (marks>=50 && marks<60){
+            System.out.println("D grade");
+        }
+        else if(marks>60 && marks<75){
+            System.out.println("B grade");
+        }else {
+            System.out.println("Invalid");
+        }
+    }
+}
