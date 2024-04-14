07280c4ae4a980101b0d53a37a39e8c66dd50688
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-14 22:15:24 +0530

    If Else

---
+++ Ggiuv.java
@@ -0,0 +1,11 @@
+class Ggiuv{
+    public static void main(String args[]){
+        int x=100;
+        int y=90;
+        if(y>x){
+            System.out.println("Y is greater than x");
+        }else{
+            System.out.println("X is greater tahn y");
+        }
+    }
+}
---
+++ Lk.java
@@ -0,0 +1,7 @@
+class Lk{
+    public static void main(String args[n]){
+        if(12>7){
+            System.out.println("12 is greater than 7");
+        }
+    }
+}
