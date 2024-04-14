ed3dd720b341408ef0b93264e393153900c2f158
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-14 22:10:23 +0530

    If else

---
+++ Lol.java
@@ -0,0 +1,7 @@
+class Lol{
+    public static void main(String args[]){
+        if(100>90){
+            System.out.println("100 is greater than 90");
+        }
+    }
+}
---
+++ Tuuuu.java
@@ -0,0 +1,11 @@
+class Tuuuu{
+    public static void main(String args[]){
+        int n=4;
+        for(int i=0;i<=4;i++){
+            for(int j=0;i<=j;j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
