9c65aef59d4869b843ab0d93afa33e0c60070e19
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-15 18:41:51 +0530

    Wap to Print *

---
+++ Zip.java
@@ -0,0 +1,10 @@
+class Zip{
+    public static void main(String args[]){
+        int n=3;
+        for(int i=3;i<=0;i++){
+            for(int j=0;j<=i;j++){
+                System.out.println("*");
+            }System.out.println();
+        }
+    }
+}
