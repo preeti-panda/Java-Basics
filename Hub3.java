f9197243b1844a73825cdcff65269e413d675a9a
preeti-panda <preetiprangyapanda@gmail.com>
2024-04-18 12:24:33 +0530

    if else Statement

---
+++ Hub3.java
@@ -0,0 +1,13 @@
+class Hub3{
+    public static void main(String args[]){
+        int a=100;
+        int b=90;
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else if(a<b){
+            System.out.println("B is greater than a");
+        }
+
+    }
+}
